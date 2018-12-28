9c707fc908827bbaf2e20894e03d21b5adb8bc16
Alexis <alexisr245@gmail.com>
2018-12-28 09:56:38 +0100

    fix: typo

--- aoscp.mk
+++ aoscp.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common AOSCP stuff.
-$(call inherit-product, vendor/asocp/configs/common_full_phone.mk)
+$(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
 
 # Inherit from h870 device
 $(call inherit-product, device/lge/h870/device.mk)
