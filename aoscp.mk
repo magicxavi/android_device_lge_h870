bbd27f35ec111b7fe9eadfe13e280b23487f33f9
Alexis <alexisr245@gmail.com>
2018-12-29 21:07:21 +0100

    refactor: use aosp_base_telephony

--- aoscp.mk
+++ aoscp.mk
@@ -16,7 +16,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common AOSCP stuff.
 $(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
