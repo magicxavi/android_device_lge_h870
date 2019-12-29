8bda78db17c045106e50732c1334a84cd3177f92
dom133 <dom133_pl@protonmail.com>
2019-12-29 23:30:30 +0100

    AEX Bringup

--- aosp_h870.mk
+++ aosp_h870.mk
@@ -16,13 +16,16 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from h870 device
 $(call inherit-product, device/lge/h870/device.mk)
+
+# Inherit some common AEX stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+
+# Boot animation res
+TARGET_BOOT_ANIMATION_RES := 2140
 
 # Security patch level
 PLATFORM_SECURITY_PATCH_OVERRIDE := 2019-05-01
@@ -31,7 +34,7 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH_OVERRIDE)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := h870
-PRODUCT_NAME := lineage_h870
+PRODUCT_NAME := aosp_h870
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-H870
 PRODUCT_MANUFACTURER := LGE
